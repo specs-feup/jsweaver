f5f769c98bd88f2ac8c4df8ce9f4903e03806589
Gil Teixeira <up201505735@fe.up.pt>
2020-10-25 21:56:36 +0000

    [jsweaver] Implement atts for common jps: MethodJp, FunctionJp, ParamJp

--- jsweaver/src/pt/up/fe/specs/jackdaw/api/JackdawWeaverApi.java
+++ jsweaver/src/pt/up/fe/specs/jackdaw/api/JackdawWeaverApi.java
@@ -3,6 +3,16 @@ package pt.up.fe.specs.jackdaw.api;
 import org.lara.interpreter.weaver.utils.LaraResourceProvider;
 
 public enum JackdawWeaverApi implements LaraResourceProvider {
+	JACKDAW_PARAM_JP("jp/JackdawParamJp.lara"),
+	JACKDAW_VAR_REF_JP("jp/JackdawVarRefJp.lara"),
+	JACKDAW_VAR_DECL_JP("jp/JackdawVarDeclJp.lara"),	
+	//JACKDAW_TYPE_JP("jp/JackdawTypeJp.lara"),
+	JACKDAW_FIELD_REF_JP("jp/JackdawFieldRefJp.lara"),
+	//JACKDAW_FIELD_JP("jp/JackdawFieldJp.lara"),
+	JACKDAW_MEMBER_CALL_JP("jp/JackdawMemberCallJp.lara"),
+	JACKDAW_CALL_JP("jp/JackdawCallJp.lara"),
+	JACKDAW_FUNCTION_JP("jp/JackdawFunctionJp.lara"),
+	JACKDAW_METHOD_JP("jp/JackdawMethodJp.lara"),
 	
 	JACKDAW_CLASS_JP("jp/JackdawClassJp.lara"),
     JACKDAW_JOIN_POINT("jp/JackdawJoinPoint.lara"),
--- jsweaver/src/pt/up/fe/specs/jackdaw/utils/JackdawCommonLanguage.java
+++ jsweaver/src/pt/up/fe/specs/jackdaw/utils/JackdawCommonLanguage.java
@@ -19,22 +19,37 @@ package pt.up.fe.specs.jackdaw.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import com.google.gson.JsonObject;
 
+import pt.up.fe.specs.jackdaw.ParentMapper;
+import pt.up.fe.specs.jackdaw.abstracts.AJackdawWeaverJoinPoint;
+
 public class JackdawCommonLanguage {
 
-	private static final Map<String, String> JOINPOINT_MAPPER;
+	private static final Map<String, Function<JsonObject, String>> JOINPOINT_MAPPER;
 	static {
 		JOINPOINT_MAPPER = new HashMap<>();
-		JOINPOINT_MAPPER.put("CallExpression", "CallJp");
-		JOINPOINT_MAPPER.put("ExpressionStatement", "ExprJp");
-		JOINPOINT_MAPPER.put("FunctionDeclaration", "FunctionJp");
-		JOINPOINT_MAPPER.put("VariableDeclaration", "DeclJp");
-		JOINPOINT_MAPPER.put("ClassDeclaration", "ClassJp");
-		JOINPOINT_MAPPER.put("Program", "FileJp");
-		JOINPOINT_MAPPER.put("Project", "ProgramJp");
-		// JOINPOINT_MAPPER.put("Joinpoint", "JoinPoint");
+
+		JOINPOINT_MAPPER.put("MethodDefinition", node -> "MethodJp");
+		JOINPOINT_MAPPER.put("Identifier", JackdawCommonLanguage::identifier);
+		JOINPOINT_MAPPER.put("VariableDeclaration", node -> "VarDeclJp");
+		JOINPOINT_MAPPER.put("MemberExpression", JackdawCommonLanguage::memberExpression);
+		JOINPOINT_MAPPER.put("CallExpression", node -> "CallJp");
+		JOINPOINT_MAPPER.put("ExpressionStatement", node -> "ExprJp");
+		JOINPOINT_MAPPER.put("FunctionDeclaration", node -> "FunctionJp");
+		JOINPOINT_MAPPER.put("ClassDeclaration", node -> "ClassJp");
+		JOINPOINT_MAPPER.put("Program", node -> "FileJp");
+		JOINPOINT_MAPPER.put("Project", node -> "ProgramJp");
+
+		// Not Supported
+		// - Field (?)
+		// - Type
+		// - Decl
+
+		// TODO: Param can also be a AssignmentPattern | BindingPattern;
+
 	}
 
 	public static String getJoinPointName(JsonObject node) {
@@ -42,7 +57,43 @@ public class JackdawCommonLanguage {
 		var mapper = JOINPOINT_MAPPER.get(type);
 		if (mapper == null)
 			return "JoinPoint";
-		return JOINPOINT_MAPPER.get(type);
+		return mapper.apply(node);
 	}
+
+	@SuppressWarnings("unused")
+	private static String identifier(JsonObject node) {
+
+		var parent = ParentMapper.getParent(node);
+
+		if (parent.has("params") && parent.getAsJsonArray("params").contains(node))
+			return "ParamJp";
+
+		// TODO: Confirm this works as intended
+		// Does not work when parent is an "If"
+		if (!parent.get("type").getAsString().equals("MemberExpression")
+				&& parent.get("type").getAsString().contains("Expr"))
+			return "VarRefJp";
+
+		return "JoinPoint";
+	}
+
+	private static String memberExpression(JsonObject node) {
+
+		var parent = ParentMapper.getParent(node);
+
+		if (parent.get("type").getAsString().equals("CallExpression") 
+				&& parent.get("callee").equals(node))
+			return "MemberCallJp";
+
+		return "FieldRefJp";
+	}
+
+//	@SuppressWarnings("unused")
+//	private static String callExpression(JsonObject node) {
+//		if (false)
+//			return "MemberCallJp";
+//
+//		return "CallJp";
+//	}
 
 }
